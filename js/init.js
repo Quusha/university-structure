fbc98702019db21bf2410391c0239306346a4423
Quusha <d.ace3@mail.ru>
2026-08-18 01:18:44 +0500

    Delete js/init.js

--- js/init.js
+++
@@ -1,8 +0,0 @@
-/* ================= INIT ================= */
-if(!load()){buildSeed()}
-ensureShape();saveLocal();
-initOrgChart();
-loadSyncCfg();
-renderAll();
-if(syncCfg.url){setStatus('sync');cloudPull().then(()=>startPoll());}else{setStatus('none');}
-
